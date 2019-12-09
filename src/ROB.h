64acb113cb1211cdec0e354afbe3e8a379c69e5f
Rinal-M <52177292+Rinal-M@users.noreply.github.com>
2019-12-09 21:18:50 +0200

    ROB h file updated

--- src/ROB.h
+++ src/ROB.h
@@ -1,22 +1,30 @@
+#include<vector>
+#include<utility>
+
 enum state { None, issued, executed, written, commited };
 
 class ROB
 {
 private:
 	int ROB_index;
-	char* Type;
-	char* Dest;
+	int Type;
+	int Dest;
 	int value;
-	bool ready; 
+	bool ready;
+	std::vector<std::pair<short*, int>>tracking;
+	//	std::deque<ROB> rob;
 public:
 	ROB();
 	//ROB(int, char*, char*, int, bool);
-	void setType(char*);
-	void setDest(char*);
+	void setType(int type_enum);
+	void setDest(int);
 	void setValue(int, int);   //set value if the status is written, enum the state
-	
-//set the ready flag if the status is written and the instruction is the top of the queue
-	void setReadinessStatus(std::deque<ROB>, int, int );
+	void reserve(short*, int, std::vector<ROB>);
+	//void setIndex(int);
+	int getIndex(std::vector<ROB>);
+	//	void FlushForBranch(bool misprediction_flag);
+	//set the ready flag if the status is written and the instruction is the top of the queue
+	void setReadinessStatus(std::vector<ROB>, int, int);
 	// takes the instruction tag in the queue of the ROB
 	bool getReadinessState();
 	// takes the instruction tag in the queue of the ROB and returns the destination to be used in the registers table
